9fdbcfe1c549fba8863b160aa5e0eb5f352a8830
Helluys <luys@live.fr>
2018-11-03 16:25:21 +0100

    Movement

---
+++ Assets/Scripts/Movement/ObjectTracker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObjectTracker : MonoBehaviour {
+
+    [SerializeField] private Transform trackedObject;
+    private Vector3 positionDelta;
+
+    // Use this for initialization
+    void Start () {
+        if (trackedObject == null) {
+            Destroy(this);
+        } else {
+            positionDelta = transform.position - trackedObject.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = trackedObject.position + positionDelta;
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public static class Direction {
+        public const int UP = 0;
+        public const int DOWN = 1;
+        public const int LEFT = 2;
+        public const int RIGHT = 3;
+    }
+
+    private const float MIN_MOVEMENT_THRESHOLD = 0.1f;
+    private readonly Quaternion INPUT_TO_WORLD = Quaternion.FromToRotation(Vector3.forward, (Vector3.forward + Vector3.left).normalized);
+
+    [SerializeField] private KeyCode[] keys;
+    [SerializeField] private float speed;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 inputDirection = Vector3.zero;
+        if (Input.GetKey(keys[Direction.UP])) {
+            inputDirection += Vector3.forward;
+        }
+
+        if (Input.GetKey(keys[Direction.DOWN])) {
+            inputDirection += Vector3.back;
+        }
+
+        if (Input.GetKey(keys[Direction.LEFT])) {
+            inputDirection += Vector3.left;
+        }
+
+        if (Input.GetKey(keys[Direction.RIGHT])) {
+            inputDirection += Vector3.right;
+        }
+
+        if (inputDirection.magnitude > MIN_MOVEMENT_THRESHOLD) {
+            inputDirection.Normalize();
+        }
+
+        Debug.Log(inputDirection);
+
+        Vector3 movement = InputToMovement(inputDirection);
+        transform.position += speed * movement * Time.deltaTime;
+        if (movement.magnitude > MIN_MOVEMENT_THRESHOLD) {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+
+    private Vector3 InputToMovement (Vector3 inputDirection) {
+        return INPUT_TO_WORLD  * inputDirection;
+    }
+
+    public void SetKey (int direction, KeyCode key) {
+        this.keys[direction] = key;
+    }
+}
