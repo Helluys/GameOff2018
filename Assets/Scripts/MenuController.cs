147185895d3bc831d59f7f11518325c2c018198e
Ben <fromain.benjamin@gmail.com>
2018-11-24 16:42:43 +0100

    Menu test

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,17 +5,74 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    [SerializeField] LeanTweenType easing;
     public Animator[] animators;
+    [SerializeField] new private Transform camera;
+
+    [SerializeField] private Transform[] cameraPos;
+    private Coroutine cGoToPos;
+
+    Vector3 currentPos;
+    Vector3 targetPos;
+    Quaternion currentAng;
+    Quaternion targetAng;
 
     private void Start()
     {
         foreach (Animator animator in animators)
             animator.SetFloat("speed", 2.0f);
+
+        GoToPos(1, 3);
     }
+
+   public void OpenInputSettings()
+   {
+        GoToPos(2, 2);
+   }
 
     public void LoadPlayScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void GoToPos(int index, float time)
+    {/*
+        if (cGoToPos != null)
+            StopCoroutine(cGoToPos);
+        cGoToPos = StartCoroutine(EGoToPos(index, time));*/
+
+        LeanTween.cancel(gameObject);
+        currentPos = camera.position;
+        targetPos = cameraPos[index].position;
+        currentAng = camera.rotation;
+        targetAng = cameraPos[index].rotation;
+        LeanTween.value(gameObject, LerpUpdate, 0, 1, 3).setEase(easing);
+    }
+
+    private void LerpUpdate(float val)
+    {
+        camera.position = Vector3.Lerp(currentPos, targetPos, val);
+        camera.rotation = Quaternion.Lerp(currentAng, targetAng, val);
+    }
+
+    private IEnumerator EGoToPos(int index, float time)
+    {
+        float elapsedTime = 0;
+        float lerpVal = 0;
+
+        Vector3 currentPos = camera.position;
+        Vector3 targetPos = cameraPos[index].position;
+        Quaternion currentAng = camera.rotation;
+        Quaternion targetAng = cameraPos[index].rotation;
+
+        while(elapsedTime < time)
+        {
+            lerpVal = elapsedTime / time;
+            camera.position = Vector3.Lerp(currentPos, targetPos, lerpVal);
+            camera.rotation = Quaternion.Lerp(currentAng, targetAng, lerpVal);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 
 
